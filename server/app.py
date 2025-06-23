3a9e16e88a43713e913ad8c286c8435e7079bdac
Allan <allanbarongo5@gmail.com>
2025-06-23 22:30:34 +0300

    Completed table relationships and serialization rules

--- server/app.py
+++ server/app.py
@@ -1,11 +1,184 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session,make_response,jsonify
 from flask_restful import Resource
-
+from sqlalchemy.exc import IntegrityError
 from config import app, db, api
-from models import User
+from models import User,Message,Booking,Destination
 
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+
+        try:
+            user = User(
+                username=data['username'],
+                admin=data['admin']
+                email=data['email']
+            )
+            user.password= data['password'] 
+
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+
+            return make_response(jsonify(user.to_dict()), 201)
+
+        except Exception as e:
+            db.session.rollback()
+            return {'error': 'There was an error while signing you up.'}, 422
+        
+class Login(Resource):
+    
+    def post(self):
+
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            if user.authenticate(password):
+
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
+    
+class Destinations(Resource):
+    def get(self):
+        destinations=[destination.to_dict() for destination in Destination.query.all()]
+        return make_response(jsonify(destinations),200)
+    def post(self):
+        data=request.get_json()
+        try:
+            destination=Destination(
+                name=data['name'],
+                country=data['country'],
+                description=data['description'],
+                image=data['image'],
+                price=data['price'],
+                activities=data['activities'],
+                message=data['message']
+            )
+            db.session.add(destination)
+            db.session.commit()
+            
+            return make_response(destination.to_dict(),201)
+        
+        except IntegrityError:
+
+            return {'error': '422 Unprocessable Entity'}, 422
+    
+class DestinationByID(Resource):
+    def get(self,id):
+        destination=Destination.query.filter(Destination.id==id).first()
+        if destination:
+            return make_response(jsonify(destination.to_dict()),200)
+        else:
+            return {'error':"Destination by that ID not found"},404
+    
+    def patch(self,id):
+        data = request.get_json()
+        destination=Destination.query.filter(Destination.id==id).first()
+        
+        for attr in data:
+            setattr(destination,attr,data[attr])
+            
+        db.session.add(destination)
+        db.session.commit()      
+        
+        return make_response(destination.to_dict(),200)
+    
+    def delete(self,id):
+        destination=Destination.query.filter(Destination.id==id).first()
+        
+        db.session.delete(destination)
+        db.session.commit()
+        
+class Bookings(Resource):
+    def get(self,id=None):
+        if id:
+            booking = Booking.query.get(id)
+            if booking:
+                return booking.to_dict(), 200
+            return {"error": "Booking not found"}, 404
+        else:
+            bookings = Booking.query.all()
+            return [b.to_dict() for b in bookings], 200
+        
+    def post(self,id=None):
+        data=request.get_json()
+        try:
+            booking=Booking(
+                user_id=data['user_id'],
+                destination_id=data['destination_id'],
+                people_count=data['people_count']
+                confirmed=data['confirmed']
+            )
+            db.session.add(booking)
+            db.session.commit()
+            
+            return make_response(booking.to_dict(),201)
+        except IntegrityError:
+
+            return {'error': '422 Unprocessable Entity'}, 422
+        
+    def patch(self,id):
+        data=request.get_json()
+        booking=Booking.query.filter(Booking.id==id)
+        
+        for attr in data:
+            setattr(booking,attr,data[attr])
+        db.session.add(booking)
+        db.session.commit()      
+        
+        return make_response(booking.to_dict(),200)
+    
+    def delete(self,id):
+        booking=Booking.query.filter(Destination.id==id).first()
+        
+        db.session.delete(booking)
+        db.session.commit()   
+        
+class Messages(Resource):
+    def get(self):
+        messages=[message.to_dict() for message in Message.query.all()]
+        return make_response(jsonify(messages),200)
+    def post(self):
+        data=request.get_json()
+        
+        try:
+            message=Message(
+                name=data['name'] 
+                email=data['email'] 
+                message=data['message'] 
+            )
+            
+            db.session.add(message)
+            db.session.commit()
+            return make_response(message.to_dict(),201)
+        
+        except IntegrityError:
+            return {'error': '422 Unprocessable Entity'}, 422
+class Logout(Resource):
+
+    def delete(self):
+
+        session['user_id'] = None
+        
+        return {}, 204
+           
+api.add_resource(Signup, "/signup")
+api.add_resource(Login, "/login")
+api.add_resource(Destinations, "/destinations")
+api.add_resource(DestinationByID, "/destinations/<int:id>")
+api.add_resource(Bookings, "/Bookings/<int:id>")
+api.add_resource(Messages, "/messages")
+api.add_resource(Logout, "/logouts")
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -25,26 +25,41 @@ class Destination(db.model,SerializerMixin):
     activities=db.Column(JSON, default=[])
     message=db.Column(db.String)
     
+    bookings=db.relationship('Booking', back_populate='destination',cascade='all, delete-orphan')
+    users=association_proxy('bookings','user',creator=lambda user_obj:Booking(user=user_obj))
+    serialize_rules = ('-bookings.destination')
+    
+    
 class Booking(db.model,SerializerMixin):
     __tablename__= 'bookings'
     
     id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String, nullable=False)
-    country=db.Column(db.String, nullable=False)
-    price=db.Column(db.Integer, nullable=False)
-    description=db.Column(db.String)
-    image=db.Column(db.String, nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'))
+    destination_id=db.Column(db.Integer, db.ForeignKey('destinations.id'))
+    people_count=db.Column(db.Integer, nullable=False, default=1)
     confirmed=db.Column(db.Boolean, default=False)
     
+    user=db.relationship('User', back_populates='bookings')
+    destination=db.relationship('Destination', back_populates='bookings')
+    serialize_rules = ('-user.bookings','-destination.bookings')
     
 class User(db.model,SerializerMixin):
     __tablename__='users'
     
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String, nullable=False,unique=True)
+    email=db.Column(db.String)
     _password=db.Column(db.String, nullable=False)
     admin=db.Column(db.Boolean, default=False)
     
+    bookings=db.relationship('Booking', back_populates='user',cascade='all, delete-orphan')
+    destinations=association_proxy('bookings','destination',creator=lambda destination_obj:Booking(destination=destination_obj))
+    
+    @validates('email')
+    def validate_email(self, key, value):
+        if '@' and '.com'not in value:
+            raise ValueError("Rating must be between 1 and 5.")
+        return value
     
     @hybrid_property
     def password(self):
@@ -60,7 +75,7 @@ class User(db.model,SerializerMixin):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
     
-class Messages(db.model,SerializerMixin):
+class Message(db.model,SerializerMixin):
     __tablename__='messages'
     
     id=db.Column(db.Integer,primary_ket=True)
