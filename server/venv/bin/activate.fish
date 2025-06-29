2030238b07598bec52dd90c87c40de60584ad262
Emmanuel Gitau <gmannu2055@gmail.com>
2025-06-29 22:39:15 +0300

    Ready to deploy to Render

--- server/venv/bin/activate.fish
+++ server/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/ashton/Desktop/travel_agency 1/Olympus_travel_agency/server/venv'
+set -gx VIRTUAL_ENV /home/emmanuel-gitau/Moringa/code/Phase-4/Olympus_travel_agency/server/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
